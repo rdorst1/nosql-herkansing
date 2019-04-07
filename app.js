3553635d2b3e4a46c2f9ca27dade66f48215c726
DESKTOP-TO6UJBG\rowan <rdorst1@avans.nl>
2019-04-07 15:05:50 +0200

    eskeetit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const MongoClient = require('mongodb').MongoClient;
 const routes = require('./routes/routes');
 const bodyParser = require('body-parser');
 const User = require('./models/User');
@@ -7,36 +8,46 @@ const app = express();
 
 mongoose.Promise = global.Promise;
 
-// if (process.env.NODE_ENV === 'production') {
-//     mongoose.connect('mongodb+srv://admin:1234@cluster0-3auhd.mongodb.net/test?retryWrites=true', {useNewUrlParser: true} );
-//     mongoose.connection.on('connected', function () {  
-//         console.log('Mongoose connected to test production database in cloud');
-//     }); 
+if (process.env.NODE_ENV === 'production') {
+    mongoose.connect('mongodb+srv://admin:1234@cluster0-3auhd.mongodb.net/test?retryWrites=true', {useNewUrlParser: true} );
+    mongoose.connection.on('connected', function () {  
+        console.log('Mongoose connected to test production database in cloud');
+    }); 
+}
+else if ( process.env.NODE_ENV === 'testCloud' ) {
+    mongoose.connect('mongodb+srv://admin:1234@cluster0-3auhd.mongodb.net/test?retryWrites=true', {useNewUrlParser: true} );
+    mongoose.connection.on('connected', function () {  
+        console.log('Mongoose connected to test cloud database');
+    }); 
+
+}else{
+    mongoose.connect('mongodb://localhost/db_local_reddit',  {useNewUrlParser: true} );
+    mongoose.connection.on('connected', function () {  
+        console.log('Mongoose connected to test database local ');
+    }); 
+}
+
+
+// if(process.env.NODE_ENV == 'production'){
+// // const MongoClient = require('mongodb').MongoClient;
+// // const uri = "mongodb+srv://admin:1234@cluster0-hg1xn.mongodb.net/test?retryWrites=true";
+// // const client = new MongoClient(uri, { useNewUrlParser: true });
+// // client.connect(err => {
+// //   const collection = client.db("test").collection("devices");
+// //   // perform actions on the collection object
+// //   console.log("Connected to mongoDb on Atlas")
+// //   client.close();
+// // });
+//   MongoClient.connect('mongodb+srv://admin:1234@cluster0-hg1xn.mongodb.net/test?retryWrites=true',{useNewUrlParser: true} ,function(err, db){
+//     if(err) throw err;
+//   });
 // }
-// else if ( process.env.NODE_ENV === 'testCloud' ) {
-//     mongoose.connect('mongodb+srv://admin:1234@cluster0-3auhd.mongodb.net/test?retryWrites=true', {useNewUrlParser: true} );
-//     mongoose.connection.on('connected', function () {  
-//         console.log('Mongoose connected to test cloud database');
-//     }); 
-
-// }else{
-//     mongoose.connect('mongodb://localhost/db_local_reddit',  {useNewUrlParser: true} );
-//     mongoose.connection.on('connected', function () {  
-//         console.log('Mongoose connected to test database local ');
-//     }); 
+// else if(process.env.NODE_ENV !== 'test'){
+//   MongoClient.connect('mongodb://localhost/db_local_reddit', {useNewUrlParser: true},function (err, db){
+//     console.log("test databse")
+//   if(err) throw err;
+//   });
 // }
-
-
-
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://admin:1234@cluster0-3auhd.mongodb.net/test?retryWrites=true";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  console.log("Connected to mongoDb on Atlas")
-  client.close();
-});
 
 
 mongoose.set('useCreateIndex', true);
