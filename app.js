a13c086e3571a88998749c2e3c38c3097f64fe04
DESKTOP-TO6UJBG\rowan <rdorst1@avans.nl>
2019-04-07 14:34:59 +0200

    Connection strings fixed.

--- app.js
+++ app.js
@@ -7,24 +7,37 @@ const app = express();
 
 mongoose.Promise = global.Promise;
 
-if (process.env.NODE_ENV === 'production') {
-    mongoose.connect('mongodb+srv://admin:Test123@cluster0-fvsbw.mongodb.net/test?retryWrites=true', {useNewUrlParser: true} );
-    mongoose.connection.on('connected', function () {  
-        console.log('Mongoose connected to test production database in cloud');
-    }); 
-}
-else if ( process.env.NODE_ENV === 'testCloud' ) {
-    mongoose.connect('mongodb+srv://admin:Test123@cluster0-fvsbw.mongodb.net/test?retryWrites=true', {useNewUrlParser: true} );
-    mongoose.connection.on('connected', function () {  
-        console.log('Mongoose connected to test cloud database');
-    }); 
+// if (process.env.NODE_ENV === 'production') {
+//     mongoose.connect('mongodb+srv://admin:1234@cluster0-3auhd.mongodb.net/test?retryWrites=true', {useNewUrlParser: true} );
+//     mongoose.connection.on('connected', function () {  
+//         console.log('Mongoose connected to test production database in cloud');
+//     }); 
+// }
+// else if ( process.env.NODE_ENV === 'testCloud' ) {
+//     mongoose.connect('mongodb+srv://admin:1234@cluster0-3auhd.mongodb.net/test?retryWrites=true', {useNewUrlParser: true} );
+//     mongoose.connection.on('connected', function () {  
+//         console.log('Mongoose connected to test cloud database');
+//     }); 
 
-}else{
-    mongoose.connect('mongodb://localhost/db_local_reddit',  {useNewUrlParser: true} );
-    mongoose.connection.on('connected', function () {  
-        console.log('Mongoose connected to test database local ');
-    }); 
-}
+// }else{
+//     mongoose.connect('mongodb://localhost/db_local_reddit',  {useNewUrlParser: true} );
+//     mongoose.connection.on('connected', function () {  
+//         console.log('Mongoose connected to test database local ');
+//     }); 
+// }
+
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://admin:1234@cluster0-3auhd.mongodb.net/test?retryWrites=true";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log("Connected to mongoDb on Atlas")
+  client.close();
+});
+
 
 mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', false);
--- neo4j-driver.js
+++ neo4j-driver.js
@@ -1,22 +1,23 @@
 const neo4j = require('neo4j-driver').v1;
 
-let driver;
+// if (process.env.NODE_ENV === 'production') {
+//     driver = neo4j.driver('bolt://hobby-igickikncggcgbkeepnhffbl.dbs.graphenedb.com:24786',
+//     neo4j.auth.basic('admin', 'b.ZHHopQJodcm2.075zXwBCfsVgXwMq'));
+//     console.log('Connected to graphendb cloud db')
+// }
+// else if ( process.env.NODE_ENV === 'testCloud' ) {
+//     driver = neo4j.driver('bolt://hobby-igickikncggcgbkeepnhffbl.dbs.graphenedb.com:24786',
+//     neo4j.auth.basic('admin', 'b.ZHHopQJodcm2.075zXwBCfsVgXwMq'));
+//     console.log('Connected to graphendb cloud db')
 
-if (process.env.NODE_ENV === 'production') {
-    driver = neo4j.driver('bolt://hobby-igickikncggcgbkeepnhffbl.dbs.graphenedb.com:24786',
-    neo4j.auth.basic('admin', 'b.ZHHopQJodcm2.075zXwBCfsVgXwMq'));
-    console.log('Connected to graphendb cloud db')
-}
-else if ( process.env.NODE_ENV === 'testCloud' ) {
-    driver = neo4j.driver('bolt://hobby-igickikncggcgbkeepnhffbl.dbs.graphenedb.com:24786',
-    neo4j.auth.basic('admin', 'b.ZHHopQJodcm2.075zXwBCfsVgXwMq'));
-    console.log('Connected to graphendb cloud db')
+// }else{
+//     driver = neo4j.driver('bolt://localhost:7687',
+//     neo4j.auth.basic('kevin', 'Test123'));
+//     console.log('Connected to local graph db')
+// }
 
-}else{
-    driver = neo4j.driver('bolt://localhost:7687',
-    neo4j.auth.basic('kevin', 'Test123'));
-    console.log('Connected to local graph db')
-}
+var driver = neo4j.driver("bolt://hobby-fiaanogldadkgbkegdcepacl.dbs.graphenedb.com:24787", neo4j.auth.basic("admin", "b.g6qmbuyClW8f.In6F5mhjiVbm7hcU"));
+console.log("Connected to Graphenedb")
 
 
 
